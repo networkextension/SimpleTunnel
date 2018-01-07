250d2cdc54cd4d18be0b8a2581465cce3702da0c
Kong XiangBo <yarshure@gmail.com>
2018-01-07 14:20:52 +0800

    use mac packtunnel test

--- mac/ViewController.swift
+++ mac/ViewController.swift
@@ -7,15 +7,132 @@
 //
 
 import Cocoa
-
+import NetworkExtension
 class ViewController: NSViewController {
 
+    var manager:NETunnelProviderManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadManager()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func save(_ sender: Any) {
+        guard let m = self.manager else {
+            fatalError()
+        }
+        
+    }
+    
+    func loadManager(){
+        NETunnelProviderManager.loadAllFromPreferences { (ms, e) in
+            print(ms)
+            if let ms = ms{
+                if ms.count != 0 {
+                    for m in ms {
+                        self.manager = m
+                    }
+                }else {
+                    self.create()
+                }
+            }
+        }
+    }
+     @IBAction func dail(_ sender: Any) {
+        _ = try! startStopToggled("")
+    }
+    func create(){
+        let config = NETunnelProviderProtocol()
+        //config.providerConfiguration = ["App": bId,"PluginType":"com.yarshure.Surf"]
+        #if os(iOS)
+            
+            config.providerBundleIdentifier = "com.yarshure.Surf.PacketTunnel"
+        #else
+            config.providerBundleIdentifier = "com.yarshure.Surf.mac.extension"
+        #endif
+        config.serverAddress = "192.168.11.9:8890"
+        
+        let manager = NETunnelProviderManager()
+        manager.protocolConfiguration = config
+       manager.localizedDescription = "Surfing"
+        
+        
+        
+        manager.saveToPreferences(completionHandler: { (error) -> Void in
+            if error != nil {
+                print(error?.localizedDescription)
+            }else {
+                self.manager = manager
+            }
+            
+        })
+    
+    }
+    func startStopToggled(_ config:String) throws ->Bool{
+        if let m = manager {
+           
+            if m.connection.status == .disconnected || m.connection.status == .invalid {
+                do {
+                    
+                    if  m.isEnabled {
+                        
+                        try m.connection.startVPNTunnel(options: [:])
+                        
+                        
+                        
+                        
+                        
+                    }else {
+                        
+                    }
+                }
+                catch let error  {
+                    throw error
+                    //mylog("Failed to start the VPN: \(error)")
+                }
+            }
+            else {
+                print("stoping!!!")
+                m.connection.stopVPNTunnel()
+            }
+        }else {
+            
+            return false
+        }
+        return true
+    }
+    @IBAction func XPC(_ sender: Any) {
+        // Send a simple IPC message to the provider, handle the response.
+        //AxLogger.log("send Hello Provider")
+        if let m = manager {
+            let me = "|Hello Provider"
+            if let session = m.connection as? NETunnelProviderSession,
+                let message = me.data(using: .utf8), m.connection.status != .invalid
+            {
+                do {
+                    try session.sendProviderMessage(message) { response in
+                        if let response = response  {
+                            if let responseString = String.init(data:response , encoding: .utf8){
+                                let list = responseString.components(separatedBy: ":")
+                                
+                                print("Received response from the provider: \(responseString)")
+                            }
+                            
+                            //self.registerStatus()
+                        } else {
+                            print("Got a nil response from the provider")
+                        }
+                    }
+                } catch {
+                    print("Failed to send a message to the provider")
+                }
+            }
+        }else {
+            print("message dont init")
+        }
+        
+    }
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
